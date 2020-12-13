39e13a7992acb7471553fe9d743ff0393d91e037
Leeingnyo <dyd1928@naver.com>
2020-12-14 08:39:28 +0900

    draw map

--- src/detect_rail_lane.py
+++ src/detect_rail_lane.py
@@ -324,14 +324,55 @@ if __name__ == "__main__":
                     speed.append(calculated_speed)
                 else:
                     speed.append(mean_speed)
-            '''
-            flow_y = flow[:,:,1]
-            flow_y[flow_y > 0] = 0
-            mean = np.mean(flow[:,:,1])
-            print(mean)
-            '''
 
         old_patch_roi = patch_roi
 
         if cv2.waitKey(33) > 0: break
+
+    cv2.destroyAllWindows()
+
+    print(len(speed))
+    print(len(direction[1:]))
+
+    global_direction = 0.0
+    D_R = 0.1
+    S_R = 0.1
+    current_x = 0
+    current_y = 0
+    positions = [(0, 0)]
+    for (s, d) in zip(speed, direction):
+        global_direction += d * D_R
+        delta_x = np.cos(global_direction) * s * S_R
+        delta_y = np.sin(global_direction) * s * S_R
+        current_x += delta_x
+        current_y += delta_y
+        positions.append((current_x, current_y))
+
+    nd_positions = np.array(positions)
+    min_x, min_y = nd_positions.min(axis=0)
+    max_x, max_y = nd_positions.max(axis=0)
+
+    nd_positions -= np.array((min_x, min_y))
+
+    MAP_WIDTH = 500
+    MAP_HEIGHT = 500
+    MAP_PAD = 20
+    Map = np.zeros((MAP_WIDTH, MAP_HEIGHT, 3))
+
+    prev_position = None
+    MAP_SCALE = 500 / max(max_x - min_x, max_y - min_y)
+    for current_position in nd_positions:
+        if prev_position is not None:
+            px = int((prev_position[0]) * MAP_SCALE)
+            py = int((prev_position[1]) * MAP_SCALE)
+            cx = int((current_position[0]) * MAP_SCALE)
+            cy = int((current_position[1]) * MAP_SCALE)
+            cv2.line(Map, (px, py), (cx, cy), (255, 255, 255), 1)
+
+        prev_position = current_position
+
+    Map = np.pad(Map, ((MAP_PAD, MAP_PAD), (MAP_PAD, MAP_PAD), (0, 0)), constant_values=0)
+
+    cv2.imshow("Map", Map)
+    cv2.waitKey()
     cv2.destroyAllWindows()
