b31150290c189d5ca5dad76eca622c9d86b11e54
Junwan <jwp1994@naver.com>
2020-12-13 16:51:25 +0900

    Add map draw

--- src/main.py
+++ src/main.py
@@ -1,11 +1,11 @@
 import numpy as np
 import cv2
 import math
-
+from PIL import Image, ImageDraw
 from detect_rail_lane import preprocess_image, dense_flow, make_roi
 
 def main():
-    print('Hello, Wolrd!')
+    print('Hello, World!')
 
     video_name = 'incheon-magnetic.mp4'
     # video_name = 'RCT3-360p.mp4'
@@ -30,14 +30,61 @@ def main():
     ret, old_frame = capture.read()
     old_gray = preprocess_image(old_frame)[0]
 
+    maparr = np.zeros((1,1))
+    idx = [0,0]
+    prevmapindex = [1000,20000]
+    mapindex = [1000,20000]
+    # img = Image.new("RGB", (40000, 40000))
+    # img1 = ImageDraw.Draw(img)
+    count = 0
+    i=0
     while capture.get(cv2.CAP_PROP_POS_FRAMES) < capture.get(cv2.CAP_PROP_FRAME_COUNT):
         capture.read()
         capture.read()
         ret, frame = capture.read()
-
         gray, _, _, canny_x, _ = preprocess_image(frame)
 
-        mask = dense_flow(old_gray, gray, frame)
+        mask, flow = dense_flow(old_gray, gray, frame)
+        speed = np.sum(np.sum(flow,axis=0),axis=0)/flow.size
+        idx[0] += speed[1]
+        idx[1] += speed[0]
+        idx = np.around(idx)
+        # img1.line([tuple(prevmapindex), tuple(mapindex)], fill ="red", width = 0) 
+        # prevmapindex = mapindex
+
+        # if count == 1000:
+        #     img.save(f"asdf_{i}.jpg")
+            # img = Image.new("RGB", (1000, 1000))
+            # img1 = ImageDraw.Draw(img)
+            # prevmapindex = [500,500]
+            # mapindex=[500,500]
+            # i+=1
+            # count = 0
+
+        x = int(idx[0])
+        y = int(idx[1])
+        try:
+            if x < 0:
+                maparr = np.append(np.zeros( (1, maparr.shape[1]) ), maparr, axis=0)
+                x += 1
+            elif x >= maparr.shape[0]:
+                maparr = np.append(maparr, np.zeros( (x - maparr.shape[0] + 1, maparr.shape[1]) ), axis=0)
+            if y < 0:
+                maparr = np.append(np.zeros( (maparr.shape[0], 1) ), maparr, axis=1)
+                y += 1
+            elif y >= maparr.shape[1]:
+                maparr = np.append(maparr, np.zeros((maparr.shape[0], y - maparr.shape[1] + 1)), axis=1)
+            maparr[x][y] = 255
+        except:
+            print(maparr.shape, x, y)
+        # np.savetxt('asdf.txt',maparr,fmt="%d")
+        count += 1
+        print(count, maparr.shape)
+        if count == 9315:
+            img=Image.fromarray(maparr)
+            img=img.convert('1')
+            np.savetxt('result.txt',maparr,fmt="%d")
+            img.save("result.png")
 
         # http://www.gisdeveloper.co.kr/?p=6714
         lines = cv2.HoughLines(make_roi(canny_x), 1, np.pi / 180, 100)
@@ -115,7 +162,10 @@ def main():
         '''
 
         if cv2.waitKey(33) > 0: break
-
+    img=Image.fromarray(maparr)
+    img=img.convert('1')
+    np.savetxt('result.txt',maparr,fmt="%d")
+    img.save("result.png")
     capture.release()
     cv2.destroyAllWindows()
 
